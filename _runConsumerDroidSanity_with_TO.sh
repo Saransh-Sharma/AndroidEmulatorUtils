67191ef63cadd38a0b49d0a3effb394a7fb57734
saransh1337 <saransh1337@localhost.localdomain>
2019-02-07 06:22:39 -0500

    add custom config for local CI for CA

--- _runConsumerDroidSanity_with_TO.sh
+++ _runConsumerDroidSanity_with_TO.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+echo "** KILLING EXISTING EMULATORS **"
+sh _emulator-kill-all.sh &
+sleep 5s
+echo "** DONE KILLING EMULATORS **"
+
 cd $CA
 git fetch
 sleep 15s
@@ -12,18 +17,17 @@ sleep 15s
 git checkout ca_config
 sleep 2s
 
-echo "** KILLING EXISTING EMULATORS **"
-sh _emulator-kill-all.sh &
-sleep 5s
-echo "** DONE KILLING EMULATORS **"
+cd $UTILS
+git fetch
+sleep 10s
 
 echo "** RE-LAUNCHING EMULATORS **"
 echo "Launching emulator Carl_Sagan"
 ./_emulator-launch.sh Carl_Sagan &
-sleep 5s
-echo "Launching emulator Sam_Harris"
-./_emulator-launch.sh Sam_Harris &
 sleep 15s
+#echo "Launching emulator Sam_Harris"
+#./_emulator-launch.sh Sam_Harris &
+#sleep 15s
 echo "** DONE LAUNCHING EMULATORS **"
 
 echo "Build and launch consumer droid sanity"
